6f89872483f784b193a32c58a72dfaf52626d283
AlyxLion <MagentaLionAlyx@gmail.com>
2025-01-17 18:35:21 +1100

    Updated
    Changed the main screen and worked out the kink

--- scripts/scriptHC.js
+++
@@ -1,6 +0,0 @@
-// Optionally, add any JS here for advanced interactions (like opening links or animations)
-document.querySelectorAll('.honeycomb-item').forEach(item => {
-    item.addEventListener('click', function() {
-        alert("You clicked on " + item.getAttribute("data-title"));
-    });
-});
